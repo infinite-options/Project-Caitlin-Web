b30347c7bdb86e1fcbb53a8cf1a256b297d3e915
tyler hu <tylerhu@yahoo.com>
2020-02-16 23:28:18 -0800

    separate branch

--- client/src/components/deleteAT.jsx
+++ client/src/components/deleteAT.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { Col } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTrashAlt, faCheck, faTimes } from '@fortawesome/free-solid-svg-icons';
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 export default class deleteAT extends Component {
 
     constructor(props) {
