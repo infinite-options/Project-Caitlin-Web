2c4894c7be6c194695b03a83e0347504f95d8de7
tyler hu <tylerhu@yahoo.com>
2020-02-22 17:51:55 -0800

    change placement of icons

---
+++ client/build/precache-manifest.e692c4d57df9745b43cb484f572266c9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4f4282d4753246fc4005941df5f5be1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e03a6e4c438190b723f",
+    "url": "/static/css/2.b90ce945.chunk.css"
+  },
+  {
+    "revision": "587a849010043d8e029c",
+    "url": "/static/css/main.bf878c98.chunk.css"
+  },
+  {
+    "revision": "7e03a6e4c438190b723f",
+    "url": "/static/js/2.11818491.chunk.js"
+  },
+  {
+    "revision": "d93228f04881b6884d8e1efde3652fd4",
+    "url": "/static/js/2.11818491.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "587a849010043d8e029c",
+    "url": "/static/js/main.175eda77.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  }
+]);
