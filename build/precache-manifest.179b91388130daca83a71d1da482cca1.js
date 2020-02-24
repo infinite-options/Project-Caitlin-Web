d35a7deb43f76cc5e34303a9403f70c43f21ccea
lymanshen <lymanshen@debian.lymanhomesystem.net>
2020-02-23 20:17:30 -0800

    Replace build files

---
+++ build/precache-manifest.179b91388130daca83a71d1da482cca1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d05b547ac3100c9d237f495ed285c90c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "61e0a3cac5587d742480",
+    "url": "/static/css/2.b90ce945.chunk.css"
+  },
+  {
+    "revision": "3dbda7caebfb9703c290",
+    "url": "/static/css/main.5bb5a495.chunk.css"
+  },
+  {
+    "revision": "61e0a3cac5587d742480",
+    "url": "/static/js/2.b3c1b0cd.chunk.js"
+  },
+  {
+    "revision": "d93228f04881b6884d8e1efde3652fd4",
+    "url": "/static/js/2.b3c1b0cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3dbda7caebfb9703c290",
+    "url": "/static/js/main.961472c1.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fbf2a9358fdb869a8d4991be729efa96.js"
+  "/precache-manifest.179b91388130daca83a71d1da482cca1.js"
 );
 
 self.addEventListener('message', (event) => {
