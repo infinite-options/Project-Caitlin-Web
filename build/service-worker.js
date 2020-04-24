b27f93026fe885b03d5e776fb92b2d25689c0135
Talin Hallajian <talinhallajian@Hrants-MacBook-Pro.local>
2020-04-23 19:17:16 -0700

    deploying

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2515decf87807bfbbd1acf677bf85bb9.js"
+  "/precache-manifest.638efdf35a26ef2a886355e73e660985.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
