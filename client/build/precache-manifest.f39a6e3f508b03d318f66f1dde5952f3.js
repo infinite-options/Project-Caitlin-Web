4d73c1316f9b187028982a7af51ea5c4a73ef679
Talin Hallajian <talinhallajian@Hrants-MBP.home>
2020-05-04 16:28:34 -0700

    trying out new credentials

---
+++ client/build/precache-manifest.f39a6e3f508b03d318f66f1dde5952f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfd995be775ba6a2470629613b8a49be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d380b954b86fc2feab04",
+    "url": "/static/css/2.b90ce945.chunk.css"
+  },
+  {
+    "revision": "a6d9c484dc2cad7ed5bc",
+    "url": "/static/css/main.1ffb1a26.chunk.css"
+  },
+  {
+    "revision": "d380b954b86fc2feab04",
+    "url": "/static/js/2.de5a6c69.chunk.js"
+  },
+  {
+    "revision": "1a7cd1fc60e73d06c55406358b398d74",
+    "url": "/static/js/2.de5a6c69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6d9c484dc2cad7ed5bc",
+    "url": "/static/js/main.7ee1991a.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  }
+]);
