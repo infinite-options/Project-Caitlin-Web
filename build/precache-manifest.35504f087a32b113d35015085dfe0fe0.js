96fbca8db17aec50bc181ed08eaccc0a0efb2f11
Talin Hallajian <talinhallajian@Hrants-MBP.home>
2020-05-04 15:29:55 -0700

    testing new credentials

---
+++ build/precache-manifest.35504f087a32b113d35015085dfe0fe0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ddad90fac7306b9f159a928daba4bd3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d380b954b86fc2feab04",
+    "url": "/static/css/2.b90ce945.chunk.css"
+  },
+  {
+    "revision": "a767030ae847108cc78a",
+    "url": "/static/css/main.1ffb1a26.chunk.css"
+  },
+  {
+    "revision": "d380b954b86fc2feab04",
+    "url": "/static/js/2.de5a6c69.chunk.js"
+  },
+  {
+    "revision": "1a7cd1fc60e73d06c55406358b398d74",
+    "url": "/static/js/2.de5a6c69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a767030ae847108cc78a",
+    "url": "/static/js/main.1cfc2912.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ed0738368ca3411da83c245828e968e0.js"
+  "/precache-manifest.35504f087a32b113d35015085dfe0fe0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
