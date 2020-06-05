73e68166b050d518099f0737b4eba9261d4ebb25
Xiaosui Zhang <maxchang39@yahoo.com>
2020-06-05 12:05:40 -0700

    change server to use automatically renewed certifcate

--- server/src/main.ts
+++ server/src/main.ts
@@ -861,8 +861,8 @@ const listener = app.listen( process.env.PORT || 80, () => {
 } );
 
 var options = {
-  key: fs.readFileSync('privatekey.pem'),
-  cert: fs.readFileSync('certificate.pem'),
+  key: fs.readFileSync('/etc/letsencrypt/live/manifestmy.space/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/manifestmy.space/fullchain.pem'),
 };
 
 https.createServer(options, app).listen(443);
