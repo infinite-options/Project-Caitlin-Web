e718810719fd40b2a228a5621c945da6d171f8a7
Pak Shing Kan <kennethkankps@gmail.com>
2020-06-20 12:58:36 -0700

    fixed type UpLoadImage -> UploadImage

---
+++ client/src/components/DefinePeriodOfTime.jsx
@@ -0,0 +1,10 @@
+import React, { Component, useState } from "react";
+import { Button, Modal } from "react-bootstrap";
+import { Form, Row, Col } from "react-bootstrap";
+import { storage } from "./firebase";
+
+export default class DefinePeriodOfTime extends Component {
+  constructor(props) {
+    super(props);
+  }
+}
