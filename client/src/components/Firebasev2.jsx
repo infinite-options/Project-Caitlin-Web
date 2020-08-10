823e752ef4ed198f01178c0e90ad0dbe52f4c59c
Talin Hallajian <talinhallajian@Hrants-MacBook-Pro.local>
2020-08-10 13:37:19 -0700

    fixed updated error with this takes me vs feature

--- client/src/components/Firebasev2.jsx
+++ client/src/components/Firebasev2.jsx
@@ -1109,11 +1109,17 @@ export default class FirebaseV2 extends React.Component {
       .collection("goals&routines")
       .doc(id);
 
+      let ATGrabFromFB = this.state.WentThroughATList;
+      ATGrabFromFB[id] = true;
+          this.setState({
+            WentThroughATList: ATGrabFromFB,
+          });
+
     ActionTaskArrayPath.get()
       .then((doc) => {
         if (doc.exists) {
           let ATExpTimeObj = this.state.AT_expected_completion_time;
-          let ATGrabFromFB = this.state.WentThroughATList;
+          // let ATGrabFromFB = this.state.WentThroughATList;
           let ATtimeCombines = 0;
           var x = doc.data()["actions&tasks"];
           if (x.length === 0) {
@@ -1126,7 +1132,9 @@ export default class FirebaseV2 extends React.Component {
             return;
           }
 
+          
           for (let k = 0; k < x.length; k++) {
+            console.log("this is k ", k);
             ActionTaskArrayPath.collection("actions&tasks")
               .doc(x[k]["id"])
               .get()
@@ -1159,6 +1167,7 @@ export default class FirebaseV2 extends React.Component {
                 // }
               });
           }
+         
         } else {
           // doc.data() will be undefined in this case
           console.log("No such document!");
